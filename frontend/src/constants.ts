16624ef959851fa68763ede651be4a06b8f7825c
Ali Naqi Al-Musawi <alinaqi2000@gmail.com>
2024-09-25 23:13:08 +0500

    Update api url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,2 +1,1 @@
-// export const API_URL = " https://taimi-todos-app.onrender.com/"
-export const API_URL = "http://192.168.1.26:5000/"
+export const API_URL = "https://taimi-todos-app.onrender.com/"
